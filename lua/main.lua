459df0dbbe9dd25af1ca310da93d7251f31f4f40
hamhub7 <hamhub7@yahoo.com>
2020-06-11 11:54:35 -0400

    TAS framework pogchamp

--- lua/main.lua
+++ lua/main.lua
@@ -1,20 +1,52 @@
+include(rootpath .. "tas.lua")
+
 controller = {}
+controllerAttached = false
 while true do
     hid_ScanInput()
-    if hid_KeyboardDown(0x1e) then
+    if hid_KeyboardDown(0x2e) and not controllerAttached then --equals
         controller = hiddbg_AttachController()
+        controllerAttached = true
     end
-    if hid_KeyboardDown(0x1f) then
+    if hid_KeyboardDown(0x2d) and controllerAttached then --minus
         hiddbg_DetachController(controller)
+        controllerAttached = false
     end
-    if hid_KeyboardDown(0x20) then
-        svc_SleepThread(1000000000)
-        controller["state"]["buttons"] = 1;
-        hiddbg_SetState(controller)
-        svc_SleepThread(1000000000)
-        controller["state"]["buttons"] = 0;
-        hiddbg_SetState(controller)
-        svc_SleepThread(1000000000)
+    if hid_KeyboardDown(0x3a) and controllerAttached then
+        runTas("script1.txt", controller)
+    end
+    if hid_KeyboardDown(0x3b) and controllerAttached then
+        runTas("script2.txt", controller)
+    end
+    if hid_KeyboardDown(0x3c) and controllerAttached then
+        runTas("script3.txt", controller)
+    end
+    if hid_KeyboardDown(0x3d) and controllerAttached then
+        runTas("script4.txt", controller)
+    end
+    if hid_KeyboardDown(0x3e) and controllerAttached then
+        runTas("script5.txt", controller)
+    end
+    if hid_KeyboardDown(0x3f) and controllerAttached then
+        runTas("script6.txt", controller)
+    end
+    if hid_KeyboardDown(0x40) and controllerAttached then
+        runTas("script7.txt", controller)
+    end
+    if hid_KeyboardDown(0x41) and controllerAttached then
+        runTas("script8.txt", controller)
+    end
+    if hid_KeyboardDown(0x42) and controllerAttached then
+        runTas("script9.txt", controller)
+    end
+    if hid_KeyboardDown(0x43) and controllerAttached then
+        runTas("script10.txt", controller)
+    end
+    if hid_KeyboardDown(0x44) and controllerAttached then
+        runTas("script11.txt", controller)
+    end
+    if hid_KeyboardDown(0x45) and controllerAttached then
+        runTas("script12.txt", controller)
     end
     svc_SleepThread(6250000)
 end
---
+++ lua/tas.lua
@@ -0,0 +1,122 @@
+buttons = {}
+buttons.A = 1;
+buttons.B = 2;
+buttons.X = 4;
+buttons.Y = 8;
+buttons.LSTICK = 16;
+buttons.RSTICK = 32;
+buttons.L = 64;
+buttons.R = 128;
+buttons.ZL = 256;
+buttons.ZR = 512;
+buttons.PLUS = 1024;
+buttons.MINUS = 2048;
+buttons.DLEFT = 4096;
+buttons.DUP = 8192;
+buttons.DRIGHT = 16384;
+buttons.DDOWN = 32768;
+
+function clearInputs(controller)
+    controller = hiddbg_SetButtons(controller, 0)
+    controller = hiddbg_SetJoystick(controller, 1, 0, 0)
+    controller = hiddbg_SetJoystick(controller, 2, 0, 0)
+
+    return controller
+end
+
+function strSplit(delim,str)
+    local t = {}
+
+    for substr in string.gmatch(str, "[^".. delim.. "]*") do
+        if substr ~= nil and string.len(substr) > 0 then
+            table.insert(t,substr)
+        end
+    end
+
+    return t
+end
+
+function scanLine(line)
+    local splitLine = strSplit(" ", line) --frame, keys, joyleft, joyright
+
+    local frame = tonumber(splitLine[1])
+
+    local keys = 0
+    if splitLine[2] ~= "NONE" then
+        local splitKeys = strSplit(";", splitLine[2])
+
+        for i,v in ipairs(splitKeys) do
+            local keystr = string.sub(v, 5)
+
+            for k in pairs(buttons) do
+                if k == keystr then
+                    keys = keys + buttons[k]
+                    break
+                end
+            end
+        end
+    end
+
+    local splitJoyLeft = strSplit(";", splitLine[3])
+    local leftdx = tonumber(splitJoyLeft[1])
+    local leftdy = tonumber(splitJoyLeft[2])
+
+    local splitJoyRight = strSplit(";", splitLine[4])
+    local rightdx = tonumber(splitJoyRight[1])
+    local rightdy = tonumber(splitJoyRight[2])
+
+
+    local controlMsg = {}
+    controlMsg["frame"] = frame
+    controlMsg["keys"] = keys
+    controlMsg["joyleft"] = {}
+    controlMsg["joyleft"]["x"] = leftdx
+    controlMsg["joyleft"]["y"] = leftdy
+    controlMsg["joyright"] = {}
+    controlMsg["joyright"]["x"] = rightdx
+    controlMsg["joyright"]["y"] = rightdy
+
+    return controlMsg
+end
+
+function runLine(controlMsg, controller)
+    hiddbg_SetButtons(controller, controlMsg["keys"])
+    hiddbg_SetJoystick(controller, 1, controlMsg["joyleft"]["x"], controlMsg["joyleft"]["y"])
+    hiddbg_SetJoystick(controller, 2, controlMsg["joyright"]["x"], controlMsg["joyright"]["y"])
+end
+
+function runTas(filename, controller)
+    local root = "sdmc:/scripts/"
+    local file = io.open(root .. filename, "r")
+    io.input(file)
+
+    local frame = 0
+    local readFile = true
+    local controlMsg = {}
+    while true do
+        if readFile then
+            local line = io.read()
+            if line == nil then break end
+            controlMsg = scanLine(line)
+        end
+
+
+        if controlMsg["frame"] == frame then
+            runLine(controlMsg, controller)
+            readFile = true
+        else
+            clearInputs(controller)
+            readFile = false
+        end
+
+        display = vi_OpenDefaultDisplay()
+        vsync_event = vi_GetDisplayVsyncEvent(display)
+        EventWaitMax(event)
+
+        frame = frame + 1
+
+    end
+
+    clearInputs(controller)
+    io.close(file)
+end
