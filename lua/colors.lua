8439be3392fcbc27bdd8aa52c475daf07aca23c3
hamhub7 <hamhub7@yahoo.com>
2020-06-12 10:52:49 -0400

    meh i will just use simple names

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,10 +1,10 @@
 local colors = {}
 
-function colors.rgba8(r, g, b, a)
+function colors.rgba(r, g, b, a)
     return ((r & 0xFF) << 0) | ((g & 0xFF) << 8) | ((b & 0xFF) << 16) | ((a & 0xFF) << 24)
 end
 
-function colors.rgb8_maxa(r, g, b)
+function colors.rgb(r, g, b)
     return colors.rgba(r, g, b, 255);
 end
 
