2757d26d0ed1e92e70e389f0597635feb423f5b7
hamhub7 <hamhub7@yahoo.com>
2020-06-17 16:15:42 -0400

    multiplayer main.lua

--- lua/main.lua
+++ lua/main.lua
@@ -9,6 +9,16 @@ local buttonsColor = colors.rgb(0, 0, 0)
 local gripLColor = colors.rgb(20, 184, 82)
 local gripRColor = colors.rgb(249, 81, 139)
 
+function waitFrames(frames)
+    local display = vi_OpenDefaultDisplay()
+    local vsync_event = vi_GetDisplayVsyncEvent(display)
+    for i=1,frames do
+        EventWaitMax(vsync_event)
+    end
+    EventClose(vsync_event)
+    vi_CloseDisplay(display)
+end
+
 function runScript(script, controller)
     local good, err = pcall(tas.runTas, root .. script .. "." .. fileext, controller)
     if not good then
@@ -28,9 +38,9 @@ function runMultiplayerScript(script, controllers)
     end
 end
 
-function allAttached(controllers)
-    for i,v in ipairs(controllers) do
-        if not hiddbg_IsControllerAttached(v) then
+function attached(ctrls, num)
+    for i=1,num do
+        if not hiddbg_IsControllerAttached(ctrls[i]) then
             return false
         end
     end
@@ -40,65 +50,173 @@ end
 local tasOn = true
 controllers = {}
 while true do
+    local ctrlCopy = {}
+    for i=1,#controllers do
+        if hiddbg_IsControllerAttached(controllers[i]) then
+            table.insert(ctrlCopy, controllers[i])
+        end
+    end
+    controllers = ctrlCopy
+
     hid_ScanInput()
 
     if tasOn then
-        if hid_KeyboardDown(hid.KeyboardScancode.F1) and hiddbg_IsControllerAttached(controllers[1]) then
-            runScript("script1", controllers[1])
-        elseif hid_KeyboardDown(hid.KeyboardScancode.F2) and hiddbg_IsControllerAttached(controllers[1]) then
-            runScript("script2", controllers[1])
-        elseif hid_KeyboardDown(hid.KeyboardScancode.F3) and hiddbg_IsControllerAttached(controllers[1]) then
-            runScript("script3", controllers[1])
-        elseif hid_KeyboardDown(hid.KeyboardScancode.F4) and hiddbg_IsControllerAttached(controllers[1]) then
-            runScript("script4", controllers[1])
-        elseif hid_KeyboardDown(hid.KeyboardScancode.F5) and hiddbg_IsControllerAttached(controllers[1]) then
-            runScript("script5", controllers[1])
-        elseif hid_KeyboardDown(hid.KeyboardScancode.F6) and hiddbg_IsControllerAttached(controllers[1]) then
-            runScript("script6", controllers[1])
-        elseif hid_KeyboardDown(hid.KeyboardScancode.F7) and hiddbg_IsControllerAttached(controllers[1]) then
-            runScript("script7", controllers[1])
-        elseif hid_KeyboardDown(hid.KeyboardScancode.F8) and hiddbg_IsControllerAttached(controllers[1]) then
-            runScript("script8", controllers[1])
-        elseif hid_KeyboardDown(hid.KeyboardScancode.F9) and hiddbg_IsControllerAttached(controllers[1]) then
-            runScript("script9", controllers[1])
-        elseif hid_KeyboardDown(hid.KeyboardScancode.F10) and hiddbg_IsControllerAttached(controllers[1]) then
-            runScript("script10", controllers[1])
-        elseif hid_KeyboardDown(hid.KeyboardScancode.F11) and hiddbg_IsControllerAttached(controllers[1]) then
-            runScript("script11", controllers[1])
-        elseif hid_KeyboardDown(hid.KeyboardScancode.F12) and hiddbg_IsControllerAttached(controllers[1]) then
-            runScript("script12", controllers[1])
-        end
-
+        -- Adding/removing controllers
         if tas.multiplayer then
-            if hid_KeyboardDown(hid.KeyboardScancode.Equal) then
+            if hid_KeyboardDown(hid.KeyboardScancode.Equal) and #controllers < 8 then
                 table.insert(controllers, hiddbg_AttachController(bodyColor, buttonsColor, gripLColor, gripRColor))
-            elseif hid_KeyboardDown(hid.KeyboardScancode.Minus) and hiddbg_IsControllerAttached(controllers[#controllers]) then
+            elseif hid_KeyboardDown(hid.KeyboardScancode.Minus) and attached(controllers, #controllers) then
                 hiddbg_DetachController(controllers[#controllers])
-            elseif hid_KeyboardDown(hid.KeyboardScancode.Num1) and allAttached(controllers) and #controllers == 2 then
-                runMultiplayerScript("script1", controllers)
             end
         else
-            if hid_KeyboardDown(hid.KeyboardScancode.Equal) and not hiddbg_IsControllerAttached(controllers[1]) then
+            if hid_KeyboardDown(hid.KeyboardScancode.Equal) and #controllers < 1 then
                 controllers[1] = hiddbg_AttachController(bodyColor, buttonsColor, gripLColor, gripRColor)
-            elseif hid_KeyboardDown(hid.KeyboardScancode.Minus) and hiddbg_IsControllerAttached(controllers[1]) then
+            elseif hid_KeyboardDown(hid.KeyboardScancode.Minus) and attached(controllers, 1) then
                 hiddbg_DetachController(controllers[1])
+            end
+        end
+
+        -- Running scripts
+        if hid_KeyboardDown(hid.KeyboardScancode.F1) then
+            if tas.multiplayer and #controllers > 1 then
+                if attached(controllers, #controllers) then
+                    runMultiplayerScript("script1", controllers)
+                end
+            else
+                if attached(controllers, 1) then
+                    runScript("script1", controllers[1])
+                end
+            end
+        elseif hid_KeyboardDown(hid.KeyboardScancode.F2) then
+            if tas.multiplayer and #controllers > 1 then
+                if attached(controllers, #controllers) then
+                    runMultiplayerScript("script2", controllers)
+                end
+            else
+                if attached(controllers, 1) then
+                    runScript("script2", controllers[1])
+                end
+            end
+        elseif hid_KeyboardDown(hid.KeyboardScancode.F3) then
+            if tas.multiplayer and #controllers > 1 then
+                if attached(controllers, #controllers) then
+                    runMultiplayerScript("script3", controllers)
+                end
+            else
+                if attached(controllers, 1) then
+                    runScript("script3", controllers[1])
+                end
+            end
+        elseif hid_KeyboardDown(hid.KeyboardScancode.F4) then
+            if tas.multiplayer and #controllers > 1 then
+                if attached(controllers, #controllers) then
+                    runMultiplayerScript("script4", controllers)
+                end
+            else
+                if attached(controllers, 1) then
+                    runScript("script4", controllers[1])
+                end
+            end
+        elseif hid_KeyboardDown(hid.KeyboardScancode.F5) then
+            if tas.multiplayer and #controllers > 1 then
+                if attached(controllers, #controllers) then
+                    runMultiplayerScript("script5", controllers)
+                end
+            else
+                if attached(controllers, 1) then
+                    runScript("script5", controllers[1])
+                end
+            end
+        elseif hid_KeyboardDown(hid.KeyboardScancode.F6) then
+            if tas.multiplayer and #controllers > 1 then
+                if attached(controllers, #controllers) then
+                    runMultiplayerScript("script6", controllers)
+                end
+            else
+                if attached(controllers, 1) then
+                    runScript("script6", controllers[1])
+                end
+            end
+        elseif hid_KeyboardDown(hid.KeyboardScancode.F7) then
+            if tas.multiplayer and #controllers > 1 then
+                if attached(controllers, #controllers) then
+                    runMultiplayerScript("script7", controllers)
+                end
+            else
+                if attached(controllers, 1) then
+                    runScript("script7", controllers[1])
+                end
+            end
+        elseif hid_KeyboardDown(hid.KeyboardScancode.F8) then
+            if tas.multiplayer and #controllers > 1 then
+                if attached(controllers, #controllers) then
+                    runMultiplayerScript("script8", controllers)
+                end
+            else
+                if attached(controllers, 1) then
+                    runScript("script8", controllers[1])
+                end
+            end
+        elseif hid_KeyboardDown(hid.KeyboardScancode.F9) then
+            if tas.multiplayer and #controllers > 1 then
+                if attached(controllers, #controllers) then
+                    runMultiplayerScript("script9", controllers)
+                end
+            else
+                if attached(controllers, 1) then
+                    runScript("script9", controllers[1])
+                end
+            end
+        elseif hid_KeyboardDown(hid.KeyboardScancode.F10) then
+            if tas.multiplayer and #controllers > 1 then
+                if attached(controllers, #controllers) then
+                    runMultiplayerScript("script10", controllers)
+                end
+            else
+                if attached(controllers, 1) then
+                    runScript("script10", controllers[1])
+                end
+            end
+        elseif hid_KeyboardDown(hid.KeyboardScancode.F11) then
+            if tas.multiplayer and #controllers > 1 then
+                if attached(controllers, #controllers) then
+                    runMultiplayerScript("script11", controllers)
+                end
+            else
+                if attached(controllers, 1) then
+                    runScript("script11", controllers[1])
+                end
+            end
+        elseif hid_KeyboardDown(hid.KeyboardScancode.F12) then
+            if tas.multiplayer and #controllers > 1 then
+                if attached(controllers, #controllers) then
+                    runMultiplayerScript("script12", controllers)
+                end
+            else
+                if attached(controllers, 1) then
+                    runScript("script12", controllers[1])
+                end
+            end
+        end
+
+        -- Utility keybinds
+        if hid_KeyboardDown(hid.KeyboardScancode.Q) and #controllers > 0 then
+            hiddbg_SetButtons(controllers[1], hid.ControllerKeys.A)
+            waitFrames(5)
+            hiddbg_SetButtons(controllers[1], 0)
+            waitFrames(5)
+        elseif hid_KeyboardDown(hid.KeyboardScancode.W) and #controllers > 0 then
+            for i,v in ipairs(controllers) do
+                hiddbg_SetButtons(v, hid.ControllerKeys.L | hid.ControllerKeys.R)
+                waitFrames(5)
+                hiddbg_SetButtons(v, 0)
+                waitFrames(5)
             end
         end
     end
 
     if hid_KeyboardDown(hid.KeyboardScancode.Esc) then
         tasOn = not tasOn
-    end
-
-    if tas.multiplayer then
-        local ctrlCopy = {}
-        for i=1,#controllers do
-            if hiddbg_IsControllerAttached(controllers[i]) then
-                table.insert(ctrlCopy, controllers[i])
-            end
-        end
-
-        controllers = ctrlCopy
     end
 
     svc_SleepThread(6250000)
