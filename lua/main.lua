aa986f9952ff26a2586ad23c2a7f70206b853531
hamhub7 <38668528+hamhub7@users.noreply.github.com>
2021-05-23 01:48:11 -0400

    yay colors

--- lua/main.lua
+++ lua/main.lua
@@ -4,10 +4,10 @@ hid = require "sys/hidenum"
 local fileext = tas.fileext
 local root = "sdmc:/scripts/"
 
-local bodyColor = colors.rgb(43, 43, 43)
-local buttonsColor = colors.rgb(0, 0, 0)
-local gripLColor = colors.rgb(20, 184, 82)
-local gripRColor = colors.rgb(249, 81, 139)
+local bodyColor = colors.rgb(103, 74, 64)
+local buttonsColor = colors.rgb(252, 174, 56)
+local gripLColor = colors.rgb(80, 263, 164)
+local gripRColor = colors.rgb(249, 82, 53)
 
 function waitFrames(frames)
     local display = vi_OpenDefaultDisplay()
